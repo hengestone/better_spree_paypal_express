81cb3685232f81ea500cfe396719bcca30cefcb7
brianthecoder <brian@20jeans.com>
2014-01-19 22:11:45 -0800

    typo

--- app/controllers/spree/paypal_controller.rb
+++ app/controllers/spree/paypal_controller.rb
@@ -39,7 +39,7 @@ module Spree
         item[:Amount][:value].zero?
       end
       pp_request = provider.build_set_express_checkout({
-        :SetExpressCheckoutRequestDetails {
+        :SetExpressCheckoutRequestDetails => {
           :ReturnURL => confirm_paypal_url(:payment_method_id => params[:payment_method_id], :utm_nooverride => 1),
           :CancelURL =>  cancel_paypal_url,
           :SolutionType => payment_method.preferred_solution.present? ? payment_method.preferred_solution : "Mark",
