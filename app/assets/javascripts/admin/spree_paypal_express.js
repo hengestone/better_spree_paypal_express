9adf4256b456690aaadb328817fedca5692eb758
Francois Harbec <fharbec@gmail.com>
2014-01-30 14:12:25 -0800

    new payment submit button is now clickable when not creating a paypal payment

--- app/assets/javascripts/admin/spree_paypal_express.js
+++ app/assets/javascripts/admin/spree_paypal_express.js
@@ -4,22 +4,22 @@ SpreePaypalExpress = {
   hideSettings: function(paymentMethod) {
     if (SpreePaypalExpress.paymentMethodID && paymentMethod.val() == SpreePaypalExpress.paymentMethodID) {
       $('.payment-method-settings').children().hide();
-      $('#payment_amount').attr('disabled', 'disabled');
-      $('button[type="submit"]').attr('disabled', 'disabled');
+	  $('#payment_amount').prop('disabled', true);
+	  $('button[type="submit"]').prop('disabled', true);
       $('#paypal-warning').show();
     } else if (SpreePaypalExpress.paymentMethodID) {
       $('.payment-method-settings').children().show();
-      $('button[type=submit]').attr('disabled', '');
-      $('#payment_amount').attr('disabled', '')
+	  $('button[type=submit]').prop('disabled', false);
+	  $('#payment_amount').prop('disabled', false)
       $('#paypal-warning').hide();
     }
   }
 }
 
 $(document).ready(function() {
-  checkedPaymentMethod = $('#payment-methods input[type="radio"]:checked');
+  checkedPaymentMethod = $('[data-hook="payment_method_field"] input[type="radio"]:checked');
   SpreePaypalExpress.hideSettings(checkedPaymentMethod);
-  paymentMethods = $('#payment-methods input[type="radio"]').click(function (e) {
+  paymentMethods = $('[data-hook="payment_method_field"] input[type="radio"]').click(function (e) {
     SpreePaypalExpress.hideSettings($(e.target));
   });
 })
