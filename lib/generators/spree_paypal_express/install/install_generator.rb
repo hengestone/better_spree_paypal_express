36a6b52d325d47c0bd32e8a059788437d91159d1
brianthecoder <brian@20jeans.com>
2014-04-13 03:32:43 -0700

    comment out frontend generator

--- lib/generators/spree_paypal_express/install/install_generator.rb
+++ lib/generators/spree_paypal_express/install/install_generator.rb
@@ -13,7 +13,7 @@ module SpreePaypalExpress
         # frontend_css_file = "vendor/assets/stylesheets/spree/frontend/all.css"
         backend_css_file = "vendor/assets/stylesheets/spree/backend/all.css"
 
-        if File.exist?(backend_css_file) && File.exist?(frontend_css_file)
+        if File.exist?(backend_css_file) #&& File.exist?(frontend_css_file)
           # inject_into_file frontend_css_file, " *= require spree/frontend/spree_paypal_express\n", :before => /\*\//, :verbose => true
           inject_into_file backend_css_file, " *= require spree/backend/spree_paypal_express\n", :before => /\*\//, :verbose => true
         end
