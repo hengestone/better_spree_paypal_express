e43b69c192e049e58bc11ea9e3102250aced044e
brianthecoder <brian@20jeans.com>
2014-06-06 12:23:31 -0700

    gonna have to give damien remedial git lessons :P

--- app/controllers/spree/paypal_controller.rb
+++ app/controllers/spree/paypal_controller.rb
@@ -4,10 +4,10 @@ module Spree
       order = current_order || raise(ActiveRecord::RecordNotFound)
       items = order.line_items.map(&method(:line_item))
 
-      tax_adjustments = order.adjustments.tax.additional + order.adjustments.tax_cloud
-      shipping_adjustments = order.adjustments.shipping
+      tax_adjustments = order.all_adjustments.tax.additional + order.adjustments.try(:tax_cloud)
+      shipping_adjustments = order.all_adjustments.shipping
 
-      order.adjustments.eligible.each do |adjustment|
+      order.all_adjustments.eligible.each do |adjustment|
         next if (tax_adjustments + shipping_adjustments).include?(adjustment)
         items << {
           :Name => adjustment.label,
@@ -116,7 +116,16 @@ module Spree
     end
 
     def payment_details(items)
-      item_sum = items.sum{|i| i[:Quantity] * i[:Amount][:value] }
+      # item_sum = items.sum { |i| i[:Quantity] * i[:Amount][:value] }
+
+      # This retrieves the cost of shipping after promotions are applied
+      # For example, if shippng costs $10, and is free with a promotion, shipment_sum is now $10
+      shipment_sum = current_order.shipments.map(&:discounted_cost).sum
+
+      # This calculates the item sum based upon what is in the order total, but not for shipping
+      # or tax.  This is the easiest way to determine what the items should cost, as that
+      # functionality doesn't currently exist in Spree core
+      item_sum = current_order.total - shipment_sum - current_order.additional_tax_total
 
       if item_sum.zero?
         # Paypal does not support no items or a zero dollar ItemTotal
